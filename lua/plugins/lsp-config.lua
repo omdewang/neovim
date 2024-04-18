13f7479e78ae807b879e6167415d8562a614800a
omdewang <omdewang31@gmail.com>
2024-04-18 19:31:48 +0530

    add lsp servers

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = {"lua_ls","tsserver"}  
+                ensure_installed = {"lua_ls","tsserver","clangd","unocss","html","jdtls","kotlin_language_server","remark_ls","pyright","sqlls"}
             })
         end
     },
@@ -19,6 +19,14 @@ return {
             local lspconfig = require("lspconfig")
             lspconfig.lua_ls.setup({})
             lspconfig.tsserver.setup({})
+            lspconfig.clangd.setup({})
+            lspconfig.unocss.setup({})
+            lspconfig.html.setup({})
+            lspconfig.jdtls.setup({})
+            lspconfig.kotlin_language_server.setup({})
+            lspconfig.remark_ls.setup({})
+            lspconfig.pyright.setup({})
+            lspconfig.sqlls.setup({})
             vim.keymap.set('n','k',vim.lsp.buf.hover,{})
             vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
             vim.keymap.set({'n','v'},'<leader>ca',vim.lsp.buf.code_action,{})
