1a6aecf4e1041ff86f9075d7268a835ebda378fe
omdewang <omdewang31@gmail.com>
2024-04-19 16:42:30 +0530

    add none-ls file

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua","javascript"},
+            auto_installed = true,
             highlight = { enable = true},
             indent = {enable = true},
             autotag = { enable = true }
